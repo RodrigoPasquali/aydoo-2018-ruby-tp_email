680e01023b27b5f1fcf16009ce2c4436f9ab8876
rodrigo <r.pasquali2412@gmail.com>
2018-06-04 11:09:10 -0300

    Se agrega test para obtener los datos de la segunda posicion de los contactos

--- tp_email/spec/contactos_spec.rb
+++ tp_email/spec/contactos_spec.rb
@@ -28,4 +28,18 @@ describe 'Contactos' do
 
     expect(valor_obtenido).to eq(valor_esperado)
   end   
+
+  it 'los datos del segundo contacto deberian ser maria gonzalez mariagonzalez@test.com' do 	
+  	nombre_esperado = 'maria' 
+  	apellido_esperado = 'gonzalez' 
+  	mail_esperado = 'mariagonzalez@test.com' 
+
+  	nombre_obtenido = contactos.get_nombre(1)
+	apellido_obtenido = contactos.get_apellido(1)
+	mail_obtenido = contactos.get_mail(1)
+
+    expect(nombre_obtenido).to eq(nombre_esperado)
+    expect(apellido_obtenido).to eq(apellido_esperado)
+    expect(mail_obtenido).to eq(mail_esperado)
+  end   
 end
