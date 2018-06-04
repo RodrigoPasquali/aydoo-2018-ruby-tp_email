ae6f1a8ab3e863ec28a598e33b996cd58d1e5d14
rodrigo <r.pasquali2412@gmail.com>
2018-06-03 21:29:16 -0300

    Se crea estructura para tp, se envia mail y se atrapa satisfactoriamente con MailCatcher

---
+++ README.md
@@ -0,0 +1,5 @@
+# aydo-2018-ruby
+
+[![Build Status](https://travis-ci.org/RodrigoPasquali/aydoo-2018-ruby)](https://travis-ci.org/RodrigoPasquali/aydoo-2018-ruby)
+
+-Rodrigo Pasquali
---
+++ tp_email/app.rb
@@ -0,0 +1,13 @@
+require 'sinatra'
+require_relative './model/email'
+
+get '/prueba/:nombre' do 
+	nombre = params[:nombre]
+	return 'hola ' + nombre
+end
+
+get '/enviar_mail/:nombre' do 
+	enviador = Email.new	
+	nombre = params[:nombre]
+	return enviador.enviar_mail(nombre)
+end
