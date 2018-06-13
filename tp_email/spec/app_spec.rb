4ae52227386a599a0b9e330770d5b8c2868ee26d
rodrigo <r.pasquali2412@gmail.com>
2018-06-13 04:16:26 -0300

    Se modifica test de app para que no rompa travis

--- tp_email/spec/app_spec.rb
+++ tp_email/spec/app_spec.rb
@@ -11,17 +11,17 @@ describe 'Aplicacion Sinatra' do
   def app
     Sinatra::Application
   end
-=begin
+
   it 'deberia obtener status 200 y el cuerpo deberia devolver "ok' do
 	  archivo_json = './data1.json'
     datos_json = File.read(archivo_json)
     content = {'Content-Type' => 'application/json'}
     post '/', datos_json, content
     cuerpo = JSON.parse(last_response.body)
-    expect(last_response).to be_ok
+    expect(last_response.status).to eq 200
     expect(cuerpo['resultado']).to eq 'ok'
   end
-=end
+
   it 'deberia obtener status 500 y el cuerpo deberia devolver "error, entrada incorrecta"' do
 	archivo_json = './data2_esquema_incorrecto.json'
     datos_json = File.read(archivo_json)
@@ -29,6 +29,7 @@ describe 'Aplicacion Sinatra' do
     post '/', datos_json, content
     cuerpo = JSON.parse(last_response.body)
     expect(last_response).not_to be_ok
+    expect(last_response.status).to eq 500
     expect(cuerpo['resultado']).to eq 'error, entrada incorrecta'
   end
 end
